558656233c6ab2b2c67cbb06ece3e05b3f613e85
danielios-hub <daniel.ios.dgp@gmail.com>
2022-01-09 11:19:27 +0100

    Extract reusable code into a helper method for checking the expected result

--- Tests/AppTests/MessagesTests.swift
+++ Tests/AppTests/MessagesTests.swift
@@ -23,49 +23,28 @@ final class MessagesTests: XCTestCase {
     
     func test_getMessages_inChatWithNoMessages_returnEmptyMessage() throws {
         let (userOne, userTwo) = try makeUsers(on: app.db)
-        let chatName = "Some chat name"
-        let tokenOne = try Token.create(user: userOne, on: app.db)
-        let chat = try makeChat(with: chatName, participants: [userOne, userTwo], on: app.db)
-        let postModel = GetMessagesInput(chatID: chat.id!)
-        
-        try app.test(.POST, messageURI(), beforeRequest: { req in
-            req.headers.bearerAuthorization = .init(token: tokenOne.value)
-            try req.content.encode(postModel)
-        }, afterResponse: { response in
-            XCTAssertEqual(response.status, .ok)
-            let messages = try response.content.decode(GenericResponse<Message.OutputList>.self).data.messages
-            XCTAssert(messages.isEmpty)
-        })
+        let chat = try makeChat(with: "Some chat name", participants: [userOne, userTwo], on: app.db)
+        try assertResult(messages: [], chat: chat, user: userOne)
     }
     
     func test_getMessages_withOneMessage_returnMessage() throws {
         let (userOne, userTwo) = try makeUsers(on: app.db)
-        let chatName = "Some chat name"
-        let tokenOne = try Token.create(user: userOne, on: app.db)
-        let chat = try makeChat(with: chatName, participants: [userOne, userTwo], on: app.db)
-        let postModel = GetMessagesInput(chatID: chat.id!)
+        let chat = try makeChat(with: "Some chat name", participants: [userOne, userTwo], on: app.db)
         let message = try Message.make(text: "some text", chat: chat, user: userOne, on: app.db)
-        
-        try app.test(.POST, messageURI(), beforeRequest: { req in
-            req.headers.bearerAuthorization = .init(token: tokenOne.value)
-            try req.content.encode(postModel)
-        }, afterResponse: { response in
-            XCTAssertEqual(response.status, .ok)
-            let messages = try response.content.decode(GenericResponse<Message.OutputList>.self).data.messages
-            XCTAssertEqual(messages, [
-                message
-            ])
-        })
+        try assertResult(messages: [message], chat: chat, user: userOne)
     }
     
     func test_getMessages_withMultipleMessages_returnMessages() throws {
         let (userOne, userTwo) = try makeUsers(on: app.db)
-        let chatName = "Some chat name"
-        let tokenOne = try Token.create(user: userOne, on: app.db)
-        let chat = try makeChat(with: chatName, participants: [userOne, userTwo], on: app.db)
-        let postModel = GetMessagesInput(chatID: chat.id!)
+        let chat = try makeChat(with: "Some chat name", participants: [userOne, userTwo], on: app.db)
         let message = try Message.make(text: "some text", chat: chat, user: userOne, on: app.db)
         let anotherMessage = try Message.make(text: "some another text", chat: chat, user: userTwo, on: app.db)
+        try assertResult(messages: [message, anotherMessage], chat: chat, user: userOne)
+    }
+    
+    private func assertResult(messages: [Message], chat: Chat, user: User) throws {
+        let tokenOne = try Token.create(user: user, on: app.db)
+        let postModel = GetMessagesInput(chatID: chat.id!)
         
         try app.test(.POST, messageURI(), beforeRequest: { req in
             req.headers.bearerAuthorization = .init(token: tokenOne.value)
@@ -73,10 +52,7 @@ final class MessagesTests: XCTestCase {
         }, afterResponse: { response in
             XCTAssertEqual(response.status, .ok)
             let messages = try response.content.decode(GenericResponse<Message.OutputList>.self).data.messages
-            XCTAssertEqual(messages, [
-                message,
-                anotherMessage
-            ])
+            XCTAssertEqual(messages, messages)
         })
     }
     
