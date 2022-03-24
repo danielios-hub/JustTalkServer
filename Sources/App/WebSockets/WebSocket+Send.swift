4905d696f9adab402a001245f1dc8420773decc6
danielios-hub <daniel.ios.dgp@gmail.com>
2022-03-24 12:02:11 +0100

    Update date encoding strategy

--- Sources/App/WebSockets/WebSocket+Send.swift
+++ Sources/App/WebSockets/WebSocket+Send.swift
@@ -8,23 +8,26 @@
 import Vapor
 
 extension WebSocket {
-  func send(_ message: SocketMessage) {
-    let encoder = JSONEncoder()
-    guard
-      let data = try? encoder.encode(message),
-      let str = String(data: data, encoding: .utf8)
-    else { return }
-    
-    send(str)
+    func send(_ message: SocketMessage) {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        
+        guard
+          let data = try? encoder.encode(message),
+          let str = String(data: data, encoding: .utf8)
+        else { return }
+        
+        send(str)
   }
     
     func send(_ message: SocketConnected) {
-      let encoder = JSONEncoder()
-      guard
-        let data = try? encoder.encode(message),
-        let str = String(data: data, encoding: .utf8)
-      else { return }
-      
-      send(str)
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+          guard
+            let data = try? encoder.encode(message),
+            let str = String(data: data, encoding: .utf8)
+          else { return }
+          
+          send(str)
     }
 }
